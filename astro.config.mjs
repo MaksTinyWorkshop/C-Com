4ec05aadfbbd2cd63598d8e603e4316796a5d3ef
MaksTinyWorkshop <makstinyworkshop@gmail.com>
2025-10-08 17:27:28 +0200

    FIX STRINGS

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { webcore } from "webcoreui/integration";
 import { rehypeBaseImages } from "./src/lib/rehypeBaseImages";
 
 const siteUrl = "https://makstinyworkshop.github.io";
-const siteBase = "/C-com";
+const siteBase = "/C-Com";
 
 export default defineConfig({
   site: siteUrl,
