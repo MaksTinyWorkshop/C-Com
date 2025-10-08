9a9b6fe957d32496871cd8818976ac59cc0be93f
MaksTinyWorkshop <makstinyworkshop@gmail.com>
2025-10-08 13:37:32 +0200

    Fix GHPAGES

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [tailwind(), mdx(), webcore()],
   // Rajout de ces deux lignes pour GithubPages
   output: 'static',
-  base: '/Nom_du_Repo',
+  base: '/CCom',
   //
   markdown: {
     syntaxHighlight: 'prism',
